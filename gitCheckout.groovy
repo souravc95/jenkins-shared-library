dfc1faf1749647216a88b04ad49ffe9da8cf269c
rabi <rabi.gayen@gmail.com>
2023-06-26 12:29:44 +0530

    delete example

--- gitCheckout.groovy
+++
@@ -1,9 +0,0 @@
-// vars/gitCheckout.groovy
-def call(Map stageParams) {
- 
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
