749f56c0e296d98f2fe3efaf08886901105df23e
rabi <rabi.gayen@gmail.com>
2023-06-28 04:49:30 +0530

    docker image

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,10 @@
+def call(String project, String ImageTag, String hubUser){
+     
+     sh """
+       docker build -t ${hubUser}/${project} .
+       docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+       docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+
+     """
+
+}
