f1cece6d8e449a9f2d558b58298c77793e537e2a
rabi <rabi.gayen@gmail.com>
2023-06-27 20:14:37 +0530

    code ana

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call (credentialsId) {
 
-    withSonarQubeEnv(credentialsId: credentialsId) {
+    withSonarQubeEnv(credentialsId: 'sonar-api') {
 
          sh 'mvn clean package sonar:sonar'
     }
