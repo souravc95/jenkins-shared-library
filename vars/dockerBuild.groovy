e36c633ecca8eb05f31229de20bbe69bc48275fb
rabi <rabi.gayen@gmail.com>
2023-06-29 21:19:29 +0530

    docker image push

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,10 +1,21 @@
-def call(String project, String ImageTag, String hubUser){
-     
-     sh """
-       docker build -t ${hubUser}/${project} .
-       docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-       docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+// Docker Image build for DockerHUB
 
-     """
+// def call(String project, String ImageTag, String hubUser){
+     
+//      sh """
+//        docker build -t ${hubUser}/${project} .
+//        docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+//        docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+
+//      """
+
+// }
+
+
+// Docker build for ECR REPO
+
+ def call(String project, String ImageTag, String hubUser){
+
+    
 
 }
