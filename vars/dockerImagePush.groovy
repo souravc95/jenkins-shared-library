76a93f82dce722e9856582da040cdf0ca2189cfa
rabi <rabi.gayen@gmail.com>
2023-06-29 20:36:20 +0530

    docker image push

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,11 +1,22 @@
 def call(String project, String ImageTag, String hubUser){
-    withCredentials([string(credentialsId: 'dockerhub', variable: 'dockerhub')]) {
 
-           sh "docker login -u rabi4450 -p ${dockerhub}"
+    // withCredentials([string(credentialsId: 'dockerhub', variable: 'dockerhub')]) {
 
-    }        
-           sh "docker image push ${hubUser}/${project}:${ImageTag}"
-           sh "docker image push ${hubUser}/${project}:latest"
+    //        sh "docker login -u rabi4450 -p ${dockerhub}"
+
+    // }  
+    withCredentials([usernamePassword(
+     credentialsId: 'dockerhubpwd', 
+     passwordVariable: 'PASS',
+     usernameVariable: 'USER'
+     )]) {
+    
+     sh "docker login -u '$USER' -p '$PASS'"
+    
+}
+
+      sh "docker image push ${hubUser}/${project}:${ImageTag}"
+      sh "docker image push ${hubUser}/${project}:latest"
     
 
 }
