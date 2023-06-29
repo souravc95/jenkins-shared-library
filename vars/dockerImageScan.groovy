fe9452ddc813f491cd3a7d6393ee2df675590904
rabi <rabi.gayen@gmail.com>
2023-06-30 02:38:27 +0530

    Error fixed

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -10,7 +10,7 @@
 
  // Image scan for ECR Repo
 
- def call(String aws_account_id, String region, String ecr-repoName){
+ def call(String aws_account_id, String region, String ecr_repoName){
 
   sh """
         trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
