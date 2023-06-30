9ca89ef95c5bf6e94c886cc3fc6fe6f1cbf6285e
rabi <rabi.gayen@gmail.com>
2023-07-01 04:27:44 +0530

    ecr repo

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -44,9 +44,9 @@
 
 def call(String aws_account_id, String region, String ecr_repoName) {
     sh """
-    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 927552331003.dkr.ecr.us-east-1.amazonaws.com
-    docker tag myecr:latest 927552331003.dkr.ecr.us-east-1.amazonaws.com/myecr:latest
-    docker push 927552331003.dkr.ecr.us-east-1.amazonaws.com/myecr:latest
+    aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+    docker tag myecr:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/myecr:latest
+    docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/myecr:latest
 
     """
 }
